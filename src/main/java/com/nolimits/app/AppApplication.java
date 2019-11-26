b772885154df12e8d8d6c81a9ad6100f4becb719
banqingyuan <15606160125@163.com>
2019-11-26 23:36:34 +0800

    添加了mybatis的配置，缕清了spring boot的工作原理 明天正式开始后端功能的开发

--- src/main/java/com/nolimits/app/AppApplication.java
+++ src/main/java/com/nolimits/app/AppApplication.java
@@ -1,12 +1,13 @@
 package com.nolimits.app;
 
-import net.bytebuddy.build.ToStringPlugin;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+@MapperScan("com.nolimits.app.mapper")
 @EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class AppApplication {
 
